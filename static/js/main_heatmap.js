4ef8e815b5eb8d73e6f493aafe430b92df618d96
Yohann Canavese <yohann.canavese@eleves.enpc.fr>
2023-11-24 11:05:16 +0100

    test une proba

--- static/js/main_heatmap.js
+++ static/js/main_heatmap.js
@@ -60,7 +60,7 @@ function fill_all(){
                     for (let j = 0; j < Runners_up.length; j++) {
                         let id = Runners_up[i] + " " + Winners[j]
                         let cell = document.getElementById(id)
-                        proba_number = resultat[q][String(G_init.index_eq_runner(G_init.index_name(Runners_up[i]), permR))+", "+G_init.index_eq_winner(G_init.index_name(Winners[j]), permC)]
+                        proba_number = resultat[q][String(G_init.index_eq_runner(G_init.index_name(change_bySpace(Runners_up[i])), permR))+", "+G_init.index_eq_winner(G_init.index_name(change_bySpace(Winners[j])), permC)]
                         cell.textContent = String(proba_number)+"%"
                     }
                 }
