cd04015ccd8f5173ca160e8a2a4ba3dd434a6869
Yohann Canavese <yohann.canavese@eleves.enpc.fr>
2023-12-16 20:11:14 +0100

    test une proba

--- static/js/main_test_isom.js
+++ static/js/main_test_isom.js
@@ -1102,7 +1102,7 @@ random_team.addEventListener("click", function(){
                 let nombre = Number(cell.textContent.slice(0, -1));// = resultat[index][index2]
                 console.log("la proba qu'on teste entre ",changeSpaceby_(runner)," et ",changeSpaceby_(name)," : ", nombre)
                 if (nombre === 0) {
-                    remove(list_button, name)
+                    remove(list_button, change_bySpace(name))
                     console.log("liste bouton juste après le remove: ",list_button)
                 }
             }
